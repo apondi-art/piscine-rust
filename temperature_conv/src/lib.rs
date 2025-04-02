0422190e726c9a415c0f6e5e0e67fda1f2b9f8c3
quochieng <aiyanarich64@gmail.com>
2025-04-02 17:48:08 +0300

    feat:add function which convert from fahrenheit to celsius and  vice versa

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f-32.0)* 0.6
+    (f - 32.0) * (5.0/9.0) 
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-   (c * 9.0/5.0) + 32.0
+    (c * 1.8) + 32.0 // No semicolon means implicit return
 }
 
 // #[cfg(test)]
