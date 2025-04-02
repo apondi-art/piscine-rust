fb9065afb966c27b8de8b31771443bbf10bdef0c
quochieng <aiyanarich64@gmail.com>
2025-04-02 08:19:49 +0300

    fix:remove datatype

--- scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d
+++
@@ -1,5 +0,0 @@
-/home/quochieng/piscines/piscine-rust/scalar/target/debug/deps/scalar-e5a77a3b9c09004d: src/lib.rs
-
-/home/quochieng/piscines/piscine-rust/scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d: src/lib.rs
-
-src/lib.rs:
