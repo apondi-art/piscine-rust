9d6709e47c55c1df30acc8037634f70a9548d74b
quochieng <aiyanarich64@gmail.com>
2025-04-02 08:13:31 +0300

    delete: delete target file

--- scalar/target/debug/deps/scalar-6a7a20d9e04272cd.d
+++
@@ -1,7 +0,0 @@
-/home/quinterabok/piscines/rust/piscine_rust/scalar/target/debug/deps/libscalar-6a7a20d9e04272cd.rmeta: src/lib.rs
-
-/home/quinterabok/piscines/rust/piscine_rust/scalar/target/debug/deps/libscalar-6a7a20d9e04272cd.rlib: src/lib.rs
-
-/home/quinterabok/piscines/rust/piscine_rust/scalar/target/debug/deps/scalar-6a7a20d9e04272cd.d: src/lib.rs
-
-src/lib.rs:
--- scalar/target/debug/deps/scalar-d8246b7349ce604c.d
+++
@@ -1,5 +0,0 @@
-/home/quinterabok/piscines/rust/piscine_rust/scalar/target/debug/deps/scalar-d8246b7349ce604c: src/lib.rs
-
-/home/quinterabok/piscines/rust/piscine_rust/scalar/target/debug/deps/scalar-d8246b7349ce604c.d: src/lib.rs
-
-src/lib.rs:
