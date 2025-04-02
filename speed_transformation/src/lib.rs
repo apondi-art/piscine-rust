09884c37aaa00226dc8a5b3df17b700b516e8e33
quochieng <aiyanarich64@gmail.com>
2025-04-02 19:03:24 +0300

    feat:function is used to convert kilometre per hour to meter per second

---
+++ speed_transformation/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+    km_h /3.6
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
