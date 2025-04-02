61542b1b142c9cb1ade3db91c729b5175defbed1
quochieng <aiyanarich64@gmail.com>
2025-04-02 22:12:29 +0300

    feat:create a struct which is in form of tuple then create function that is can be used to access struct values

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,25 @@
+pub struct Student(u32, String, String);
+
+// Implement functions to access the fields
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> &str {
+    &student.1
+}
+
+pub fn last_name(student: &Student) -> &str {
+    &student.2
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
