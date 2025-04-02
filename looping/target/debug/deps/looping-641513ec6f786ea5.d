6b1bfd85c383727a3910591062a6aba459d60e13
quochieng <aiyanarich64@gmail.com>
2025-04-02 20:48:43 +0300

    feat:create function which takes standard input and compare as loop proceed

--- looping/target/debug/deps/looping-641513ec6f786ea5.d
+++
@@ -1,5 +0,0 @@
-/home/quinterabok/piscines/rust/piscine-rust/looping/target/debug/deps/looping-641513ec6f786ea5: src/main.rs
-
-/home/quinterabok/piscines/rust/piscine-rust/looping/target/debug/deps/looping-641513ec6f786ea5.d: src/main.rs
-
-src/main.rs:
