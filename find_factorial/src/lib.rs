cb5bbf2944d9370e14ea4f1559128a10ac2e6280
quochieng <aiyanarich64@gmail.com>
2025-04-02 21:24:00 +0300

    feat:create a factorial function

---
+++ find_factorial/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn factorial(num: u64) -> u64 {
+    let mut result = 1;
+    for i in 2..=num {
+        result *= i;
+    }
+    result
+}}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
