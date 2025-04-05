37b8f456fd6e907e862182f1c225f2906de9d833
quochieng <aiyanarich64@gmail.com>
2025-04-05 20:35:25 +0300

    feat:add  function change_brightness which uses struct light , the initializes it with method  new the - it loops over lights and check if is equal to the alias then assign light to brightness

---
+++ changes/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+    pub alias: String,
+    pub brightness: u8,
+}
+
+impl Light {
+    pub fn new(alias: &str) -> Self {
+        Light {
+            alias: alias.to_string(),
+            brightness: 0,
+        }
+    }
+}
+
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+    for light in lights {
+        if light.alias == alias {
+            light.brightness = value;
+            break;
+        }
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
