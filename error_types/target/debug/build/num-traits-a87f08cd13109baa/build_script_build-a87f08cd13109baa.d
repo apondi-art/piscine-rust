5eea34255140b71da2b923948341c4571d44373b
quochieng <aiyanarich64@gmail.com>
2025-04-09 00:56:10 +0300

    fix

--- error_types/target/debug/build/num-traits-a87f08cd13109baa/build_script_build-a87f08cd13109baa.d
+++
@@ -1,5 +0,0 @@
-/home/quinterabok/piscines/piscine-rust/error_types/target/debug/build/num-traits-a87f08cd13109baa/build_script_build-a87f08cd13109baa: /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/quinterabok/piscines/piscine-rust/error_types/target/debug/build/num-traits-a87f08cd13109baa/build_script_build-a87f08cd13109baa.d: /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs:
--- error_types/target/debug/deps/iana_time_zone-8b5aa34ede04db6f.d
+++
@@ -1,7 +0,0 @@
-/home/quinterabok/piscines/piscine-rust/error_types/target/debug/deps/libiana_time_zone-8b5aa34ede04db6f.rmeta: /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs
-
-/home/quinterabok/piscines/piscine-rust/error_types/target/debug/deps/iana_time_zone-8b5aa34ede04db6f.d: /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs /home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs
-
-/home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs:
-/home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs:
-/home/quinterabok/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs:
