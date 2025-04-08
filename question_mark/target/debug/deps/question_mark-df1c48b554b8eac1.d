fc9877b89a42f1e95ab4897d1d9276dc8aa71d1d
quochieng <aiyanarich64@gmail.com>
2025-04-08 23:48:05 +0300

    feat: implement nested Option structures with get_fourth_layer method
    - Added structs One, Two, Three, and Four with nested Option fields
    - Implemented get_fourth_layer for One to retrieve the u16 value from Four
    - Used the ? operator for clean and efficient Option chaining

---
+++ question_mark/target/debug/deps/question_mark-df1c48b554b8eac1.d
@@ -0,0 +1,5 @@
+/home/quinterabok/piscines/piscine-rust/question_mark/target/debug/deps/libquestion_mark-df1c48b554b8eac1.rmeta: src/lib.rs
+
+/home/quinterabok/piscines/piscine-rust/question_mark/target/debug/deps/question_mark-df1c48b554b8eac1.d: src/lib.rs
+
+src/lib.rs:
---
+++ question_mark/target/debug/deps/question_mark-e5d6b270ae5de247.d
@@ -0,0 +1,5 @@
+/home/quinterabok/piscines/piscine-rust/question_mark/target/debug/deps/libquestion_mark-e5d6b270ae5de247.rmeta: src/lib.rs
+
+/home/quinterabok/piscines/piscine-rust/question_mark/target/debug/deps/question_mark-e5d6b270ae5de247.d: src/lib.rs
+
+src/lib.rs:
