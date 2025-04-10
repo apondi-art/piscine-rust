ab7673251a3cf09c6dc7c4cb57223194c7619e09
quochieng <aiyanarich64@gmail.com>
2025-04-10 23:57:58 +0300

    feat: add expected_variable function with case and edit distance checks
    - Implement function to check if a string is in camel or snake case
    - Perform case-insensitive comparison using edit distance
    - Return similarity percentage if above 50%, otherwise None
    - Include Levenshtein distance calculation for string similarity

---
+++ expected_variable/target/debug/deps/expected_variable-0e43ad8e0ec1b087.d
@@ -0,0 +1,5 @@
+/home/quinterabok/piscines/piscine-rust/expected_variable/target/debug/deps/libexpected_variable-0e43ad8e0ec1b087.rmeta: src/lib.rs
+
+/home/quinterabok/piscines/piscine-rust/expected_variable/target/debug/deps/expected_variable-0e43ad8e0ec1b087.d: src/lib.rs
+
+src/lib.rs:
---
+++ expected_variable/target/debug/deps/expected_variable-298a2a01de624b04.d
@@ -0,0 +1,5 @@
+/home/quinterabok/piscines/piscine-rust/expected_variable/target/debug/deps/libexpected_variable-298a2a01de624b04.rmeta: src/lib.rs
+
+/home/quinterabok/piscines/piscine-rust/expected_variable/target/debug/deps/expected_variable-298a2a01de624b04.d: src/lib.rs
+
+src/lib.rs:
