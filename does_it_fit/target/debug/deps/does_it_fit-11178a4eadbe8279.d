ccd9ef9aa62db15abaa641557eae9f7671fdbf8e
quochieng <aiyanarich64@gmail.com>
2025-04-10 22:01:32 +0300

    chamged my file name adding s after volumes

--- does_it_fit/target/debug/deps/does_it_fit-11178a4eadbe8279.d
+++
@@ -1,6 +0,0 @@
-/home/quinterabok/piscines/piscine-rust/does_it_fit/target/debug/deps/libdoes_it_fit-11178a4eadbe8279.rmeta: src/lib.rs src/areas_volumes.rs
-
-/home/quinterabok/piscines/piscine-rust/does_it_fit/target/debug/deps/does_it_fit-11178a4eadbe8279.d: src/lib.rs src/areas_volumes.rs
-
-src/lib.rs:
-src/areas_volumes.rs:
