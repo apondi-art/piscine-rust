fa0e3ae5a5ef3d35a998add2717294f40a33d654
quochieng <aiyanarich64@gmail.com>
2025-04-26 01:42:25 +0300

    feat: add Roman numeral conversion system
    - Implement RomanDigit enum and RomanNumber struct
    - Complete u32 conversion in both directions
    - Handle all special cases (4, 9, etc.)
    - Fix syntax issues and edge cases

---
+++ roman_numbers_iter/src/lib.rs
@@ -0,0 +1,90 @@
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RomanDigit {
+    Nulla,
+    I,
+    V,
+    X,
+    L,
+    C,
+    D,
+    M,
+}
+
+impl From<u32> for RomanDigit {
+    fn from(n: u32) -> Self {
+        match n {
+            1..=4 => RomanDigit::I,
+            5..=9 => RomanDigit::V,
+            10..=49 => RomanDigit::X,
+            50..=99 => RomanDigit::L,
+            100..=499 => RomanDigit::C,
+            500..=999 => RomanDigit::D,
+            1000..=5000 => RomanDigit::M,
+            _ => RomanDigit::Nulla,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RomanNumber(pub Vec<RomanDigit>);
+
+impl From<u32> for RomanNumber {
+    fn from(n: u32) -> Self {
+        if n == 0 {
+            return RomanNumber(vec![RomanDigit::Nulla]);
+        }
+
+        let mut quotient = n;
+        let mut place = 0;
+        let mut reverse_roman = Vec::new();
+
+        while quotient != 0 {
+            let remainder = quotient % 10;
+            quotient /= 10;
+            place += 1;
+            
+            match remainder {
+                4 => {
+                    reverse_roman.push(RomanDigit::from(10_u32.pow(place)/2));
+                    reverse_roman.push(RomanDigit::from(10_u32.pow(place - 1)));
+                },
+                9 => {
+                    reverse_roman.push(RomanDigit::from(10_u32.pow(place)));
+                    reverse_roman.push(RomanDigit::from(10_u32.pow(place - 1)));
+                },
+                5..=8 => {
+                    for _ in 0..(remainder - 5) {
+                        reverse_roman.push(RomanDigit::from(10_u32.pow(place - 1)));
+                    }
+                    reverse_roman.push(RomanDigit::from(10_u32.pow(place)/2));
+                },
+                1..=3 => {
+                    for _ in 0..remainder {
+                        reverse_roman.push(RomanDigit::from(10_u32.pow(place - 1)));
+                    }
+                },
+                _ => {},
+            }
+        }
+
+        reverse_roman.reverse();
+        RomanNumber(reverse_roman)
+    }
+}
+
+impl RomanNumber {
+    pub fn to_u32(&self) -> u32 {
+        self.0.iter().fold(0, |acc, digit| {
+            acc + match digit {
+                RomanDigit::Nulla => 0,
+                RomanDigit::I => 1,
+                RomanDigit::V => 5,
+                RomanDigit::X => 10,
+                RomanDigit::L => 50,
+                RomanDigit::C => 100,
+                RomanDigit::D => 500,
+                RomanDigit::M => 1000,
+            }
+        })
+    }
+}
